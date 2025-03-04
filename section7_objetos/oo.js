81fe368f2f20ca85f52aab3bac3524f6239cb06c
Arthur Fontinele <arthurfontis17@gmail.com>
2025-03-04 17:31:54 -0300

    OO 04/03/2025

---
+++ section7_objetos/oo.js
@@ -0,0 +1,25 @@
+//código não executável
+
+//procedural
+processamento(valor1, valor2, valor3)
+
+//OO (letra o)
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() //foco passou a a ser o objeto
+
+//Princípios Importante:
+//1. abstração - resumir para a situação necessário, somente focando no essencial
+
+//2. encapsulamento - interface de comunicação para o usuário, não precisa passar TODOS os detalhes para a interface
+
+//3. herança(prototype)
+
+//4. polimorfismo - tem uma genérica e pode atribuir sub tipos
