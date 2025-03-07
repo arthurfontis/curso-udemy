d5322f97aa4db87a3aabd3db28f9d0e7ea7eb130
Arthur Fontinele <arthurfontis17@gmail.com>
2025-03-07 10:00:28 -0300

    07/03/2025

---
+++ section7_objetos/aula10_heranca03.js
@@ -0,0 +1,21 @@
+const pai = { nome: 'Pedro', corCabelo: 'Preto'}
+
+const filha1 = Object.create(pai)
+filha1.nome = 'Ana'
+console.log(filha1.corCabelo)
+
+const filha2 = Object.create(pai, {
+    nome: {value: "Bia", writable: false, enumerable: true}
+})
+
+console.log(filha2.nome)
+filha2.nome = 'Carla'
+console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`)
+delete filha2.nome
+console.log(Object.keys(filha1))
+console.log(Object.keys(filha2))
+
+for(let key in filha2){
+    filha2.hasOwnProperty(key) ? //saber se o atributo é do proprio obj ou por herança
+    console.log(key) : console.log(`por herança: ${key} `)
+}
