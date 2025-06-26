df8350eabd71963ae711f9824c8bea52c3fe6cf0
Arthur Fontinele <arthurfontis17@gmail.com>
2025-06-26 19:19:00 -0300

    26/06/25

--- section21_react/exercicios/src/index.js
+++ section21_react/exercicios/src/index.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React  from "react" 
+
+//destructuring {Fragment, Component}
 import ReactDOM from "react-dom/client"
 
-import Primeiro from "./componentes/primeiro"
+import Multi , {BoaNoite} from "./componentes/Multiplos"
+
+//import {BoaNoite, BoaTarde} from "./componentes/Multiplos"
+//import BomDia from "./componentes/BomDia"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 // $('<h1>').html('React') -- tipo isso
-root.render(<Primeiro />)
+root.render(
+    <div>
+        <Multi.BoaTarde nome="Ana" />
+        <BoaNoite nome="Bia" />
+    </div>
+)
