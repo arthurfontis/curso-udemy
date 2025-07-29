1548ab828d4cd59e049bc8c9ab007736bc8bf6b7
Arthur Fontinele <arthurfontis17@gmail.com>
2025-07-29 11:16:04 -0300

    29/07/25

---
+++ aulao/fundamentos/src/app/api/clientes/route.js
@@ -0,0 +1,27 @@
+export async function GET(request) {
+    return new Response(JSON.stringify({
+        message: 'GET funcionando',
+        metodo: request.method
+    }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
+export async function POST(request) {
+    return new Response(JSON.stringify({
+        message: 'POST funcionando',
+        metodo: request.method
+    }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
+export async function PUT(request) {
+    return new Response(JSON.stringify({
+        message: 'PUT funcionando',
+        metodo: request.method
+    }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
--- aulao/fundamentos/src/app/api/hello/route.js
+++ aulao/fundamentos/src/app/api/hello/route.js
@@ -9,14 +9,13 @@ export async function GET(request) {
 }
 
 export async function POST(request) {
-  const data = await request.json();
 
   return new Response(JSON.stringify({
     message: 'POST funcionando',
-    dados: data,
     metodo: request.method
   }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' }
   });
 }
+
