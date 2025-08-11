882f13572437667639d3320f6be52974ad82a7e6
Arthur Fontinele <arthurfontis17@gmail.com>
2025-08-11 19:22:05 -0300

    11/08

---
+++ section26_vue/calculator/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <h1>Calculadora</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "RobotoMono";
+    src: url(fonts/RobotoMono-Thin.ttf);
+    }
+
+*{
+    font-family: "RobotoMono", monospace;
+}
+
+body{
+    margin: 0px;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: white;
+    background: linear-gradient( to right, #4ECDC4,#556270);
+}
+</style>
